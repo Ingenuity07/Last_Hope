217e86ca93dc7a47e32a9e17bf6dd9a90b386ec1
Ingenuity07 <shivamsi103@gmail.com>
2022-05-18 11:21:52 +0530

    path added in index

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bodyParser=require('body-parser')
 const passport=require('passport')
 const session = require('express-session');
 const cors = require('cors')
-
+const path = require('path')
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended : true}));
